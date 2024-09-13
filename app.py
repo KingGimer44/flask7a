fd012f14e31dfe37818246a388237a7dae914abf
KingGimer44 <150565033+KingGimer44@users.noreply.github.com>
2024-09-12 20:53:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,64 @@ from flask import render_template
 from flask import request
 import pusher
 
+import mysql.connector
+import datetime
+import pytz
+
+con = mysql.connector.connect(
+    host="185.232.14.52",
+    database="u760464709_tst_sep",
+    user="u760464709_tst_sep_usr",
+    password="dJ0CIAFF="
+)
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    com.close()
     return render_template("app.html")
 
 @app.route("/alumnos")
 def alumnos():
+    com.close()
     return render_template("alumnos.html")
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
+    com.close()
     matricula = request.form["txtMatriculaFA"];
     nombreapellido = request.form["txtNombreApellidoFA"];
     return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+
+@app.route("/buscar")
+def buscar():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+    cursor.execute("SELECT * tst0_reservas")
+    
+    registros = cursor.fetchall()
+
+    return registros;
     
 @app.route("/evento", methods=["GET"])
 def evento():
+    if not con.is_connected():
+        con.reconnect()
+
+    cursor = con.cursor()
+
+    args = request.args
+
+    sql = "INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora) VALUES (%s, %s, %s)"
+    val = (args["temperatura"], args["humedad"], datetime.datetime.now())
+    cursor.execute(sql, val)
+    
+    con.commit()
+    com.close()
+    
     pusher_client = pusher.Pusher(
         app_id='1767967',
         key='34091ea15b1a362fb38d',
